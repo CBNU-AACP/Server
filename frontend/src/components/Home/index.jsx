9f0bbad2ad3faeb58d8d184f993678f64fe2aef3
동동이 <drdragon777@naver.com>
2021-05-13 15:28:12 +0900

    Delete frontend directory

--- frontend/src/components/Home/index.jsx
+++
@@ -1,7 +0,0 @@
-import StyledHome from "./style";
-
-function Home() {
-  return <StyledHome>Hi!</StyledHome>;
-}
-
-export default Home;
--- frontend/src/components/Home/style.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Home = styled.div`
-  font-size: 5rem;
-`;
-
-export default Home;
