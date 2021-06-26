036d1146dbeba696b95f402b07a91c67ecfad623
DongWooE <ehddn977@naver.com>
2021-06-26 18:09:23 +0900

    [ADD] - (#3) add model for member

---
+++ models/member.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Member extends Sequelize.Mode {
+    static init(sequelize){
+        return super.init({
+            name : {
+                type: Sequelize.STRING(20),
+                allowNull : false,
+            },
+            sid : {
+                type: Sequelize.INTEGER,
+                allowNull : true,
+            },
+        },{
+            sequelize,
+            timestamps : true,
+            underscored : false,
+            modelName : 'Member',
+            tableName : 'models',
+            paranoid : false,
+            charset : 'utf8',
+            collate : 'utf8_general_ci',
+            });
+    }
+};
