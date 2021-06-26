2c87fb296e18f9146907e9e8c36f4c48c5e3aa43
DongWooE <ehddn977@naver.com>
2021-06-26 18:19:42 +0900

    [ADD] - (#3) add db settings in index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development'; 
 const config = require('../config/index')[env];
+const Member = require('./member');
+const Class = require('./class');
 
 const db = {};
 
@@ -12,5 +14,11 @@ const sequelize = new Sequelize(
 );
 
 db.sequelize = sequelize;
+
+db.Member = Member;
+db.Class = Class;
+
+Member.init(sequelize);
+Class.init(sequelize);
 
 module.exports = db;
