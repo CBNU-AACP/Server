b46deb125b18952c75b372bd4b3a0e43dcf59611
DongWooE <ehddn977@naver.com>
2021-06-26 18:11:04 +0900

    [ADD] - (#3) add model for class

---
+++ models/class.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+
+module.exports = class CLass extends Sequelize.Mode {
+    static init(sequelize){
+        return super.init({
+            name : {
+                type: Sequelize.STRING(20),
+                allowNull : false,
+            },
+            cid : {
+                type: Sequelize.INTEGER,
+                allowNull : true,
+            },
+        },{
+            sequelize,
+            timestamps : true,
+            underscored : false,
+            modelName : 'Class',
+            tableName : 'classes',
+            paranoid : false,
+            charset : 'utf8',
+            collate : 'utf8_general_ci',
+            });
+    }
+};
