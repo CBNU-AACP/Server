5c4c4e7a384bba169020223197156f9b5b2c78de
DongWooE <ehddn977@naver.com>
2021-07-10 02:28:15 +0900

    [ADD] (#8) - add putCourse router in course

--- api/routes/v1/courses/index.js
+++ api/routes/v1/courses/index.js
@@ -3,6 +3,7 @@ const controller = require('./controller');
 const router = Router();
 
 router.post('/:userId', controller.createCourse);
+router.put('/:courseId', controller.putCourse);
 router.delete('/:courseId', controller.deleteCourse);
 
 module.exports = router;
