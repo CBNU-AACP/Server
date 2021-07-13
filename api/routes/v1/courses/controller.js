0159fd72fd03daa13b4ec6c9cf44332a6c495ce0
DongWooE <ehddn977@naver.com>
2021-07-14 00:40:44 +0900

    [ADD] (#16) - fixing error in making courseId

--- api/routes/v1/courses/controller.js
+++ api/routes/v1/courses/controller.js
@@ -5,7 +5,7 @@ const createCourse = async(req,res,next)=>{
     try {
         const user = await User.findOne({where:{userId}});
         if(!user) res.send("fail");
-        let courseId = calcCourseId(user.count, userId);
+        let courseId = createCourseId(user.count, user.name);
         const course = await Course.create({name,courseId,userId});       //body에는 name만 담기게 된다
         await user.addCourse(course);
         res.json(course);
@@ -41,16 +41,16 @@ const deleteCourse = async(req,res,next)=>{
     }
 }
 
-const calcCourseId = (count,userId)=>{
-    let date = new Date();
-    let month = date.getMonth()+1;
-    if(month/10 == 0) month = "0" + month;
-    let transCount = count;
-    for(let i = 10; i< 1000; i*=10){
-        if(count/i == 0) transCount = "0" + transCount;
-    }
-    let result = date.getFullYear()+""+month+""+count+"_"+userId;
-    return result;
+const createCourseId = (count,courseName)=>{
+    const date = new Date();
+    let year = date.getFullYear().toString();       //현재 년도 받아오기
+    let month = date.getMonth() + 1;                //현재 month 받아오기
+    month = month < 10 ? '0' + month.toString() : month.toString();     //month를 2자리 수로 만들기
+    let name = (courseName+"").charCodeAt(0);         //현재 과목명의 아스키코드 받아오기
+    let milie = date.getMilliseconds().toString();      
+    milie = milie.length >= 3 ? milie : new Array(4-milie.length).join("0")+milie;        //현재의 밀리세컨드를 받아오고 3자리가 될때까지 9을 붙여준다
+    let expandCount = count < 10 ? '0' + count.toString() : count.toString();     //과목의 개수 받아오고 2자리 수로 만들기    
+    return year + month + name + milie + expandCount;
 }
 
 module.exports = {createCourse, putCourse, deleteCourse};
