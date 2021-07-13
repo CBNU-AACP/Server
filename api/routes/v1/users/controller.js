63de4654194ae6a044f47503641a5af1eace7c99
L-o-g-a-n <xy.logandev@gmail.com>
2021-07-14 00:47:27 +0900

    [ADD] (#22) - adding a validating correct password function

--- api/routes/v1/users/controller.js
+++ api/routes/v1/users/controller.js
@@ -8,24 +8,34 @@ const YOUR_SECRET_KEY = process.env.SECRET_KEY;
 const createToken = async function(req, res, next) {
   const { userId, userPassword } = req.body;
   try {
-    const user = await User.findOne({ where: {userId, userPassword} })
+    const user = await User.findOne({ where: {userId} });
     if(user == null){
       res.status(400).json({
         message: 'fail' //POST 요청이 실패했다는 의미로 400 상태와 함께 실패 메세지를 보낸다.
       })
     }
     else{
-      const token = jwt.sign({
-        userId: user.userId
-      }, YOUR_SECRET_KEY, {
-        expiresIn: '1h'
-      });
-
-      res.cookie('user', token);  //client 쿠키쪽에 user라는 이름의 토큰 값을 쿠키로 저장!
-      res.status(201).json({  //POST 요청이 성공했다는 의미로 201 상태와 함께 성공 메세지와 token을 보낸다.
-        message: 'success',
-        token
-      });
+      bcrypt.compare(userPassword, user.userPassword, function(err, res2) {
+        if(res2===true)
+        {
+          const token = jwt.sign({
+          userId: user.userId
+          }, YOUR_SECRET_KEY, {
+          expiresIn: '1h'
+          });
+          res.cookie('user', token);  //client 쿠키쪽에 user라는 이름의 토큰 값을 쿠키로 저장!
+          res.status(201).json({  //POST 요청이 성공했다는 의미로 201 상태와 함께 성공 메세지와 token을 보낸다.
+          message: 'success',
+          message2: 'login success',
+          token
+          });
+        } else {
+          res.status(400).json({
+          message: 'fail', //POST 요청이 실패했다는 의미로 400 상태와 함께 실패 메세지를 보낸다.
+          message2: 'login fail'
+          })
+        }
+      })
     }
   } catch (error) {
     console.error(error);
@@ -35,10 +45,10 @@ const createToken = async function(req, res, next) {
 
 
 const createUser = async function(req, res, next) {
-  try {//ID 중복 걸러주기
+  try {
     const saltRounds = 10;
     const user = await User.findOne({where: { userId: req.body.userId }})
-    if(user) {
+    if(user) {  //userId 중복 걸러주기.
       res.status(400).json({
         message: 'fail', //POST 요청이 실패했다는 의미로 400 상태와 함께 실패 메세지를 보낸다.
         message2: 'Duplicated userId'
@@ -57,17 +67,6 @@ const createUser = async function(req, res, next) {
         message: 'success',
       });
     }
-    // const user = await User.create(req.body);
-    // bcrypt.genSalt(saltRounds, function(err, salt) {
-    //     if(err) return next(err);
-    //     bcrypt.hash(user.userPassword, salt, function(err, hash) {
-    //       if(err) return next(err);
-    //       user.update({ userPassword: hash })
-    //     });
-    // });
-    // res.status(201).json({  //회원 가입에 성공하면 201 상태와 함께 성공 메세지를 보낸다.
-    //   message: 'success',
-    // });
   } catch (error) {
     console.error(error);
     next(error);
