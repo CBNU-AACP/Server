5000836d36d910e4d0bb538da55ec3e1e67f6245
DongWooE <ehddn977@naver.com>
2021-07-14 23:46:45 +0900

    [ADD] (#23) - add controllers and router modify the model of courseDate

--- api/routes/v1/index.js
+++ api/routes/v1/index.js
@@ -4,10 +4,12 @@ const memberRouter = require('./members');
 const courseRouter = require('./courses');
 const userRouter = require('./users');
 const memberListRouter = require('./memberLists');
+const courseDateRouter = require('./courseDates');
 
 router.use('/members', memberRouter);
 router.use('/courses', courseRouter);
 router.use('/users', userRouter);
 router.use('/memberLists', memberListRouter);
+router.use('/courseDates', courseDateRouter);
 
 module.exports = router;
