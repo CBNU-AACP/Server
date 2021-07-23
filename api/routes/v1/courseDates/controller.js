b74343266ba19d74bc8bc5cf70f322facc202b99
L-o-g-a-n <xy.logandev@gmail.com>
2021-07-23 19:15:35 +0900

    [FIX] (#32) - remove duplicated members

--- api/routes/v1/courseDates/controller.js
+++ api/routes/v1/courseDates/controller.js
@@ -15,13 +15,16 @@ const findOrCreateCourseDate = async(req,res,next)=>{
         const users = await memberList.getUsers();
 
         users.forEach(async element => {
-          let member = await Member.create();
-          await courseDate[0].addMember(member);
-          await element.addMember(member);
-          await memberList.addMember(member);
+          const checkDuplicatedUser = await element.getMembers({where: {courseDateId}});
+          if(checkDuplicatedUser.length == 0) { //이미 똑같은 courseDateId로 각각의 User들에 대한 Member가 존재하는 경우에 대한 중복 방지를 위한 처리과정
+            let member = await Member.create();
+            await courseDate[0].addMember(member);
+            await element.addMember(member);
+            await memberList.addMember(member);
+          }
         });
-
-        res.json(courseDate);
+        
+        return res.json(courseDate);
     } catch (error) {
         console.error(error);
         next(error);
