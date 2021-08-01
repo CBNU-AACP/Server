3ae355b8456c627063243ed56244f028d5fcb394
DongWooE <ehddn977@naver.com>
2021-08-01 16:30:51 +0900

    [RER] (#42) optimizing index.js in routes & model

--- models/index.js
+++ models/index.js
@@ -1,14 +1,9 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development'; 
 const config = require('../config/index')[env];
-const Member = require('./member');
-const Course = require('./course');
-const CourseDate = require('./courseDate');
-const MemberList = require('./memberList');
-const User = require('./user');
-// sequelize 객체를 생성하는 코드입니다
+const {readdirSync} = require('fs');
+const {join} = require('path');
 
-// 먼저 sequelize 객체를 .env 정보에 기반하여 생성합니다
 const sequelize = new Sequelize(
   config.database, 
   config.username, 
@@ -16,27 +11,21 @@ const sequelize = new Sequelize(
   config
 );
 
-//db 객체에 각종 정보들을 넣어줍니다
 const db = {};
 db.sequelize = sequelize;
-db.Member = Member;
-db.Course = Course;
-db.CourseDate = CourseDate;
-db.MemberList = MemberList;
-db.User = User;
 
-Member.init(sequelize);
-Course.init(sequelize);
-CourseDate.init(sequelize);
-MemberList.init(sequelize);
-User.init(sequelize);
+readdirSync(join(__dirname, 'schemas'))
+  .filter(file=>/\.js/.test(file))    //js로 끝나는 것만 test에 넣어서 true를 리턴해 filter로 거른다
+  .forEach(file=>{
+    const fileName = file.replace(/\.js/, '').replace(/\b[a-z]/, letter => letter.toUpperCase())    //파일명에서 js를 뺴주고 첫글자를 대문자로 replace한다
+    const schema = require(join(__dirname, 'schemas', file));   //해당 model에 해당하는 것을 require해준다
+    db[fileName] = schema;   
+    schema.init(sequelize);
+    if(fileName == 'Course') schema.changeCount(db);    //Course에는 따로 changeCount라는 메소드가 있으므로 
+  });
 
-Member.associate(db);
-Course.associate(db);
-CourseDate.associate(db);
-MemberList.associate(db);
-User.associate(db);
-
-Course.changeCount(db);
+Object.keys(db).forEach((key)=>{ 
+    if(key != 'sequelize') db[key].associate(db);   //associate도 위에서 처리하려고 했지만 이전에 부모 관계에 있는 DB가 init처리가 되지않아서 오류가 발생, 그래서 뒤로 따로 뺏음
+});
 
 module.exports = db;
