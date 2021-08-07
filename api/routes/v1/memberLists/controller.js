f2f2643949bbb35ed1fd847d30efc22dc457c95f
L-o-g-a-n <xy.logandev@gmail.com>
2021-08-07 23:02:33 +0900

    [FIX] (#45) - fix to register as a recently entered user

--- api/routes/v1/memberLists/controller.js
+++ api/routes/v1/memberLists/controller.js
@@ -10,7 +10,7 @@ const createMemberList = async(req,res,next)=>{
         const course = await Course.findByPk(courseId);
         const memberList = await MemberList.create();
         await course.setMemberList(memberList);
-        res.json(createResponse(res, memberList));
+        res.json(memberList);
     } catch (error) {
         console.error(error);
         next(error);
@@ -24,15 +24,16 @@ const enrollMembers = async(req,res,next)=>{
         const course = await Course.findByPk(courseId);
         let memberList = await course.getMemberList();        //memberList를 바로 가져오지않는 이유는 memberList의 아이디를 따로 클라쪽에서 저장하고 있어야하기 때문이다
         if(!memberList) memberList = await axios.get(`${SERVER_URL}/v1/memberLists/${courseId}`);
+        const previousUser = await memberList.getUsers(); //최근에 들어온 members 배열로 덮어씌우기 위해서 이전에 존재했던 관계를 모두 찾아 지운다.
+        await previousUser.forEach(async function(item) { await item.removeMemberList(memberList) });
         members.sort();     //다시 재정렬해준다
         const existed = await Promise.all(        //findByPk로 user를 동시에 착고 응답을 다 받으면 existed에 넣는다
             members.map((item)=>{
-                return User.findByPk(item);         
+              return User.findByPk(item);         
             })
         )
-        const count = existed.length;     //response로 개수 정보도 함께 넘겨준다
         await memberList.addUsers(existed.map(item=>item));      //item에 다른 값들은 제외하고 userId로 찾는다
-        res.json(createResponse(res, {count}));      
+        res.json(createResponse(res, existed));      
     } catch (error) {
         console.error(error);
         next(error);
