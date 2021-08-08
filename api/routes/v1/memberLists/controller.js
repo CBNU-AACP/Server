bf5343efca893146077e0765b0d6e3c2376a9e3d
L-o-g-a-n <xy.logandev@gmail.com>
2021-08-08 16:30:29 +0900

    [RFR] (#45) - refactoring code about removeUsers

--- api/routes/v1/memberLists/controller.js
+++ api/routes/v1/memberLists/controller.js
@@ -24,15 +24,13 @@ const enrollMembers = async(req,res,next)=>{
         const course = await Course.findByPk(courseId);
         let memberList = await course.getMemberList();        //memberList를 바로 가져오지않는 이유는 memberList의 아이디를 따로 클라쪽에서 저장하고 있어야하기 때문이다
         if(!memberList) memberList = await axios.get(`${SERVER_URL}/v1/memberLists/${courseId}`);
-        const previousUser = await memberList.getUsers(); //최근에 들어온 members 배열로 덮어씌우기 위해서 이전에 존재했던 관계를 모두 찾아 지운다.
-        await previousUser.forEach(async function(item) { await item.removeMemberList(memberList) });
         members.sort();     //다시 재정렬해준다
         const existed = await Promise.all(        //findByPk로 user를 동시에 착고 응답을 다 받으면 existed에 넣는다
             members.map((item)=>{
-              return User.findByPk(item);         
+              return User.findByPk(item);   
             })
         )
-        await memberList.addUsers(existed.map(item=>item));      //item에 다른 값들은 제외하고 userId로 찾는다
+        await memberList.setUsers(existed.map(item=>item));      //최근에 들어온 배열로 덮어씌우기 위함
         res.json(createResponse(res, existed));      
     } catch (error) {
         console.error(error);
