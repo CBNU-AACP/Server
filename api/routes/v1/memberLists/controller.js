27a0da9a63c877e0de9083cbd219dd919aa333bd
L-o-g-a-n <xy.logandev@gmail.com>
2021-08-19 16:43:39 +0900

    [FIX] (#58) - reconstruct memberLists routers

--- api/routes/v1/memberLists/controller.js
+++ api/routes/v1/memberLists/controller.js
@@ -9,9 +9,13 @@ const createMemberList = async(req,res,next)=>{
     const {params:{courseId}} = req;
     try {
         const course = await Course.findByPk(courseId);
-        const memberList = await MemberList.create();
-        await course.setMemberList(memberList);
-        res.json(memberList);
+        let memberList = await course.getMemberList();
+        if(!memberList) {
+          memberList = await MemberList.create();
+          await memberList.setCourse(course);
+        }
+        const users = await memberList.getUsers({attributes: ['userId', 'name', 'studentId']});
+        res.json(createResponse(res, users));
     } catch (error) {
         console.error(error);
         next(error);
@@ -24,7 +28,6 @@ const enrollMembers = async(req,res,next)=>{
     try {
         const course = await Course.findByPk(courseId);
         let memberList = await course.getMemberList();        //memberList를 바로 가져오지않는 이유는 memberList의 아이디를 따로 클라쪽에서 저장하고 있어야하기 때문이다
-        if(!memberList) memberList = await axios.get(`${SERVER_URL}/v1/memberLists/${courseId}`);
         members.sort();     //다시 재정렬해준다
         const existed = await Promise.all(        //findByPk로 user를 동시에 착고 응답을 다 받으면 existed에 넣는다
             members.map((item)=>{
@@ -39,22 +42,7 @@ const enrollMembers = async(req,res,next)=>{
     }
 }
 
-const searchUsers = async(req, res, next) => {
-  const {params: {courseId}} = req;
-  try {
-    const course = await Course.findByPk(courseId);
-    if(!course) next(COURSE_NOT_FOUND);
-    const memberList = await course.getMemberList();
-    if(!memberList) next(MEMBERLIST_NOT_FOUND);
-    const users = await memberList.getUsers({attributes: ['userId', 'name', 'studentId']});
-    res.json(createResponse(res, users));
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-};
-
 const deleteMembers = async(req,res,next)=>{
         //body에 넣을지 query에 넣을지 고민해봐야함
 }
-module.exports = {createMemberList, enrollMembers, searchUsers};
+module.exports = {createMemberList, enrollMembers};
--- api/routes/v1/memberLists/index.js
+++ api/routes/v1/memberLists/index.js
@@ -4,6 +4,5 @@ const router = Router();
 
 router.get('/:courseId', controller.createMemberList);
 router.post('/:courseId', controller.enrollMembers);
-router.get('/search/:courseId', controller.searchUsers);
 
 module.exports = router;
