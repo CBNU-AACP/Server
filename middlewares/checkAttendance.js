0224900916cdbd3b24be35b1ee02d36b5334e30f
L-o-g-a-n <xy.logandev@gmail.com>
2021-08-23 17:57:28 +0900

    [RFR] (#64) - refactoring an attendance rotuer

--- middlewares/checkAttendance.js
+++ middlewares/checkAttendance.js
@@ -4,18 +4,20 @@ const { USER_NOT_FOUND, INVALID_QR_CODE, INVALID_COURSE_DATE_ID } = require('../
 const checkAttendance = async (req, res, next) => {
   const { userId, validNum, courseDateId } = req.body;
   const user = await User.findByPk(userId);
-  if (user == null) {
-    next(USER_NOT_FOUND);
+  try {
+    if (!user)
+      next(USER_NOT_FOUND);
+    if (user.validNum != validNum)  //사용자가 보내온 QR code가 유효하지 않은 경우
+      next(INVALID_QR_CODE);
+    const courseDate = await CourseDate.findByPk(courseDateId);
+    if (!courseDate) 
+      next(INVALID_COURSE_DATE_ID);
+    res.locals.courseDate = courseDate;
+    next();
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
-  if (user.validNum != validNum) { //사용자가 보내온 QR code가 유효하지 않은 경우
-    next(INVALID_QR_CODE);
-  }
-  const courseDate = await CourseDate.findByPk(courseDateId);
-  if (courseDate == null) {
-    next(INVALID_COURSE_DATE_ID);
-  }
-  res.locals.courseDate = courseDate;
-  next();
 };
 
 exports.checkAttendance = checkAttendance;
