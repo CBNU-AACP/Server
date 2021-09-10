dc6148fe8d53437f6921a7c66b07d8a801246b72
DongWooE <ehddn977@naver.com>
2021-09-10 19:26:18 +0900

    [REF] (#82) - refactoring the access-token middleware

--- errors/index.js
+++ errors/index.js
@@ -11,13 +11,13 @@ const errors = {
   INVALID_QR_CODE: [400, '유효하지 않은 QR Code입니다.'],
   INVALID_COURSE_DATE_ID: [400, '유효하지 않은 courseDateId입니다.'],
   INVALID_PASSWORD: [400, '유효하지 않은 비밀번호입니다.'],
-  EXPIRED_TOKEN : [400, '만료된 Token입니다.'],
+  TOKEN_EXPIRED : [400, '만료된 Token입니다.'],
 
   // 401 Errors
   LOGIN_REQUIRED: [401, '로그인이 필요합니다.'],
 
   // 403 Errors
-  FORBIDDEN: [403, '권한이 없는 요청입니다.'],
+  JSON_WEB_TOKEN_ERROR : [403, '권한이 없는 요청입니다.'],
 
   // 404 Errors
   USER_NOT_FOUND: [404, '찾을 수 없는 유저입니다.'],
