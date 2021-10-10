9ce2ce4936546803b4c25a1ce8a3b792067e8675
DongWooE <ehddn977@naver.com>
2021-10-10 17:39:43 +0900

    [DOC] add some remarks

--- api/socket/attend/index.js
+++ api/socket/attend/index.js
@@ -4,18 +4,18 @@ module.exports = async(io) => {
     const attend = io.of('/attend');        // socket.Io 객체를 가지고 네임 스페이스 생성
     attend.on('connection', (socket)=>{     // 소켓이 연결되었다면 실행
         console.log(`new client entered attend space ${socket.id}`);
-        socket.on('check', (data)=>{
+        socket.on('check', (data)=>{        //ValidNum을 checking해주는 이벤트 처리
             updateAttendance(data.userId, data.validNum, data.courseDateId)
                 .then((data)=> {
                     console.log(data);
-                    socket.emit('message', {"code" : "success"})
+                    socket.emit('message', {"code" : "success"})        //message 네임 스페이스에 해당 문구를 출력
                 })
                 .catch((err)=>{
                     console.log(err);
                     socket.emit('message', {"code" : err})
                 });
         })
-        socket.on('change', (data)=>{
+        socket.on('change', (data)=>{       //validNum을 갱신시켜주는 이벤트  처리
             updateValidNum(data.userId, data.validNum)
                 .then((data)=>{
                     console.log(data);
